8fefb5842c333d0a9c0977b9af3e597b7c33b7b7
TangraGames <40128312+TangraGames@users.noreply.github.com>
2024-12-29 12:24:32 -0800

    Moved drawing to its own function
    Moved drawing to its own function

--- poisson-disk-sampling/src/main.rs
+++ poisson-disk-sampling/src/main.rs
@@ -6,11 +6,26 @@ use macroquad::{
 
 mod poisson_disk_sampling;
 
+const WINDOW_WIDTH:i32 = 800;
+const WINDOW_HEIGHT:i32 = 600;
+
+fn draw_points(points: &Vec<(f32, f32)>, min_radius: f32) {
+    let radius_color:Color = Color::new(0.9, 0.16, 0.22, 0.7);
+
+    for point in points.iter() {
+	// draw circle around each point with half radius
+        draw_circle(point.0, point.1, min_radius/2.0, radius_color);
+	// draw circle on point
+        draw_circle(point.0, point.1, 2.0, LIGHTGRAY);
+    }
+}
+
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "Poisson Disk Sampling".to_owned(),
-        window_width: 600,
-        window_height: 400,
+        window_width: WINDOW_WIDTH,
+        window_height: WINDOW_HEIGHT,
         high_dpi: true,
         window_resizable: false,
         fullscreen: false,
@@ -20,18 +35,13 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-	let min_radius: f32 = 25.0f32;
-    let region = (800.0f32, 600.0f32);
+    let min_radius: f32 = 25.0f32;
+    let region: (f32, f32) = (WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32);
     let points:Vec<(f32, f32)> = poisson_disk_sampling::generate_points(min_radius, region);
 
     loop {
         clear_background(BLACK);
-        let radius_color:Color = Color::new(0.9, 0.16, 0.22, 0.7);
-
-        for point in points.iter() {
-            draw_circle(point.0, point.1, min_radius/2.0, radius_color);
-            draw_circle(point.0, point.1, 4.0, LIGHTGRAY);
-        }      
+        draw_points(&points, min_radius);
         next_frame().await
     }
 }
